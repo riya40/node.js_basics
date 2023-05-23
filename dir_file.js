a2fb1191acdb8ccdfba7b73d03ec38647b9b23fc
riya40 <priyanunna20@gmail.com>
2023-05-23 14:59:18 +0530

    Added:print the current directory/file program

---
+++ dir_file.js
@@ -0,0 +1,9 @@
+// GLOBALS  - NO WINDOW !!!!
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+
+console.log(__dirname) //just directory path 
+console.log(__filename) //the filename with the directory path . 
